a663a4aa27ab2883dbb8f2a22aed587b4b086e72
Rubel Foridi <rubelforidi@gmail.com>
2023-01-19 22:58:05 +0530

    day-3

---
+++ script/index.js
@@ -0,0 +1,10 @@
+let signinDiv = document.getElementsByClassName("signinDiv")[0];
+let signin = document.getElementsByClassName("signin")[0];
+
+signinDiv.addEventListener("mouseover", function(){
+  signin.style.display = "block";
+});
+
+signinDiv.addEventListener("mouseout", function(){
+  signin.style.display = "none";
+});
