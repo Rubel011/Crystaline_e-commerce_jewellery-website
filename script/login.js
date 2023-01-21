ab92c67caf909ecb7cc2c5998bcb2f02ec45bd86
Madhura Tatte <madhuratatte95@gmail.com>
2023-01-21 20:24:26 +0530

    day-5 tasks completed

--- script/login.js
+++ script/login.js
@@ -1,1 +1,21 @@
+var storedData=JSON.parse(localStorage.getItem("signUpData"));
 
+    document.querySelector("#submit").addEventListener("click",check);
+
+    function check(){
+        var enter=document.querySelector("#enter").value;
+        
+        let flag=false;
+
+        for(let i=0;i<storedData.length;i++){
+            if(storedData[i].email===enter || storedData[i].mobile===enter){
+                flag=true;
+            }
+        }
+        if(flag==true){
+            alert("Login Successful!!");
+            window.location.href="index.html";
+        }else{
+            alert("Login Failed!");
+        }
+    }
---
+++ script/signUp.js
@@ -0,0 +1,50 @@
+document.querySelector("#submit").addEventListener("click",register);
+
+var signUp=JSON.parse(localStorage.getItem("signUpData"))||[];
+
+function register(e){
+   e.preventDefault();
+   
+   var obj={
+       mobile:document.querySelector("#mobileNo").value,
+       email:document.querySelector("#email").value,
+       firstName:document.querySelector("#Fname").value,
+       lastName:document.querySelector("#Lname").value,
+       password:document.querySelector("#password").value,
+       verifyPassword:document.querySelector("#verifyPass").value,
+       gender:document.querySelector("#radio").value,
+   }
+   
+   
+
+   // for checking passwords match
+   let password=document.querySelector("#password");
+   let verifyPassword=document.querySelector("#verifyPass");
+   if(password.value != verifyPassword.value){
+       alert("Passwords do not match!!");
+       document.querySelector("#password").value="";
+       document.querySelector("#verifyPass").value="";
+       return;
+   }else{
+       alert("Welcome to CrystaLane!!");
+       window.location.href="login.html";
+       
+   }
+
+   // for checking the length of mobile number
+   let mobile=document.querySelector("#mobileNo")
+   if (mobile.value.length !=10){
+       alert("Invalid Mobile Number!!");
+       document.querySelector("#mobileNo").value="";
+       return;
+   }
+
+   
+   
+   signUp.push(obj);
+   
+   localStorage.setItem("signUpData",JSON.stringify(signUp));
+   
+
+  
+}
