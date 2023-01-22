4c34b63287199a32b20d48fff050fa587985eff9
Rubel Foridi <rubelforidi@gmail.com>
2023-01-22 15:06:11 +0530

    admin page

--- script/admin.js
+++ script/admin.js
@@ -116,13 +116,22 @@ let product_id = document.getElementById("product_id");
 let product_image = document.getElementById("product_image");
 let product_name = document.getElementById("product_name");
 let product_price = document.getElementById("product_price");
+let add_product = document.getElementById("add_product");
+let product_type = document.getElementById("product_type");
+let product_type_label = document.getElementById("product_type_label");
 
 
 form.style.display = "none"
+product_type.style.display = "none"
+product_type_label.style.display = "none"
 let data = JSON.parse(localStorage.getItem("favourites")) || [];
 
 products_btn.addEventListener("click", () => {
   change_dash.innerText = "Products"
+  fetchData()
+
+})
+function fetchData() {
   fetch('https://63c6ab94d307b769673e3b21.mockapi.io/rings')
     .then((res) => {
       return res.json();
@@ -143,13 +152,7 @@ products_btn.addEventListener("click", () => {
     .catch(function (error) {
       console.log(error);
     })
-
-
-
-
-
-
-})
+}
 function displayproducts(data) {
   prod_container.innerHTML = null;
   data.forEach((element, index) => {
@@ -171,16 +174,20 @@ function displayproducts(data) {
 
     edit.addEventListener('click', () => {
       form.style.display = "block"
+      product_type.style.display = "block"
+      product_type_label.style.display = "block"
       product_id.value = element.id
       product_name.value = element.name
       product_price.value = element.price
       product_image.value = element.image
+      product_type.value = element.type
 
     })
     del.addEventListener("click", function () {
-      data.splice(index, 1);
-      localStorage.setItem("favourites", JSON.stringify(data));
-      displayproducts(data);
+      // data.splice(index, 1);
+      // localStorage.setItem("favourites", JSON.stringify(data));
+      // displayproducts(data);
+      deleteProduct(element.type, element.id)
     })
 
 
@@ -192,16 +199,95 @@ function displayproducts(data) {
 
 }
 
+add_product.addEventListener("click", () => {
+  form.style.display = "block"
+  product_type.style.display = "block"
+  product_type_label.style.display = "block"
+
+})
+
 main_form.addEventListener("submit", (e) => {
   e.preventDefault();
   let obj = {
+    id: main_form.product_id.value,
     name: main_form.product_name.value,
     image: main_form.product_image.value,
-    id: main_form.product_id.value,
     price: main_form.product_price.value,
+    type: main_form.product_type.value
   }
-  console.log(obj)
+  if (main_form.priority.value == "add") {
+    addProduct(obj)
+  } else if (main_form.priority.value == "update") {
+    updateProduct(obj)
+  }
+  // console.log(obj)
 })
+
+
+function addProduct(obj) {
+  console.log( JSON.stringify(obj))
+  fetch(`https://63c6ab94d307b769673e3b21.mockapi.io/${obj.type}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(obj)
+    }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      fetchData()
+      console.log(data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+}
+function updateProduct(obj) {
+  // console.log('update')
+  fetch(`https://63c6ab94d307b769673e3b21.mockapi.io/${obj.type}/${obj.id}`,
+    {
+      method: 'PATCH',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(obj)
+    }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      fetchData()
+      console.log(data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+}
+function deleteProduct(type,id) {
+  // console.log('update')
+  fetch(`https://63c6ab94d307b769673e3b21.mockapi.io/${type}/${id}`,
+    {
+      method: 'DELETE'
+
+    }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      fetchData()
+      console.log(data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+}
+
 
 function displayEarrings(data) {
   earring_container.innerHTML = null;
