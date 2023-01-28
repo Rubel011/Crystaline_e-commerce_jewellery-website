0944f3d031f532feec34b9d90fa1c83914e6725c
Madhura Tatte <madhuratatte95@gmail.com>
2023-01-28 10:07:52 +0530

    login input password section added

--- script/login.js
+++ script/login.js
@@ -4,18 +4,24 @@ var storedData=JSON.parse(localStorage.getItem("signUpData"));
 
     function check(){
         var enter=document.querySelector("#enter").value;
-        
-        let flag=false;
+        var password=document.querySelector("#password").value;
+        let flag1=false;
+        let flag2=false;
 
         for(let i=0;i<storedData.length;i++){
-            if(storedData[i].email===enter || storedData[i].mobile===enter){
-                flag=true;
+            if(storedData[i].email===enter  && storedData[i].password===password){
+                flag1=true;
+                flag2=true;
+            }
+            else if( storedData[i].mobile===enter && storedData[i].password===password){
+                flag1=true;
+                flag2=true;
             }
         }
-        if(flag==true){
+        if(flag1==true && flag2==true){
             alert("Login Successful!!");
             window.location.href="index.html";
         }else{
-            alert("Login Failed!");
+            alert("Login Failed! Please Try Again!!");
         }
     }
